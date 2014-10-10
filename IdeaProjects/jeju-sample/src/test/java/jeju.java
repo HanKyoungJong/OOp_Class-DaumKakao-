b4ac3645479c2d7096545500fbaccca97fe90aa7
HKJ <valunas713@google.com>
2014-10-10 17:17:55 +0900

    jeju

---
+++ IdeaProjects/jeju-sample/src/test/java/jeju.java
@@ -0,0 +1,16 @@
+import com.sun.corba.se.spi.transport.CorbaAcceptor;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+public class jeju {
+    @Test
+    public void testAdd() throws Exception {
+
+      assertThat(1 + 1, is(2));
+
+    }
+}
